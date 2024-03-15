86d740ea73ceeb6dcdac0d16f539775d2e26a17b
Nikhil S <nikhilsnayak3473@gmail.com>
2024-03-15 21:51:49 +0530

    add cn

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,12 @@
+import { cn } from '../utils/ui';
+
 export function Button({ children, onClick, className }) {
   return (
     <button
-      className={
+      className={cn(
+        'px-2 py-1 bg-slate-800 text-slate-50 rounded-sm',
         className
-          ? `px-2 py-1 bg-slate-800 text-slate-50 rounded-sm ${className}`
-          : 'px-2 py-1 bg-slate-800 text-slate-50 rounded-sm'
-      }
+      )}
       onClick={onClick}
     >
       {children}
