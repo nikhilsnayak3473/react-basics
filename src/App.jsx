19a3ade2a97d7b32c57f40f8eb6521c75e14d1ac
Nikhil S <nikhilsnayak3473@gmail.com>
2024-03-15 21:39:26 +0530

    remove routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Navbar } from './components/layout/navbar';
 import { Home } from './pages/home';
-import { About } from './pages/about';
-import { PageNotFound } from './pages/not-found';
+
 export default function App() {
-  const route = window.location.href.replace('http://localhost:5173', '');
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main className='mt-4'>
-        {/* Conditional Rendering */}
-        {route === '/' ? (
-          <Home />
-        ) : route === '/about' ? (
-          <About />
-        ) : (
-          <PageNotFound />
-        )}
+        <Home />
       </main>
     </>
   );
